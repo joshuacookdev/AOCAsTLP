7e09ed944ab4edebe3722895c400b0a5c461d313
Joshua Cook <jdcook3@outlook.com>
2020-12-03 07:47:33 -0600

    Day Three

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace DayThree
+Console.WriteLine(ChallengeOne("Sample.txt"));
+Console.WriteLine(ChallengeOne("Input.txt"));
+Console.WriteLine(ChallengeTwo("Sample.txt"));
+Console.WriteLine(ChallengeTwo("Input.txt"));
+
+
+static int ChallengeOne(string filepath)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    List<string> data = BuildStringList(filepath);
+    return GetSpaceCounts(data,3,1).treeCount;
 }
+
+static long ChallengeTwo(string filepath)
+{
+    List<string> data = BuildStringList(filepath);
+    HashSet<(int,int)> input = new(){ (1,1), (3,1), (5,1), (7,1), (1,2) };
+    long product = 1;
+    foreach ((int,int) set in input)
+    {
+        product *= GetSpaceCounts(data, set.Item1, set.Item2).treeCount;
+    }
+    
+    return product;
+}
+
+static (int treeCount, int clearCount) GetSpaceCounts(List<string> data, int right, int down)
+{
+    int treeCount = 0, clearCount = 0, rowCount = 0, colCount = 0;
+    int rowLength = data.First().Length;
+
+    do
+    {
+        int col = colCount % rowLength;
+        var row = data[rowCount];
+        char spot = row.ElementAt(col);
+        if (spot == '.') clearCount++;
+        else treeCount++;
+        rowCount += down;
+        colCount += right;
+    } while (rowCount < data.Count());
+    return (treeCount, clearCount);
+
+}
+
+static List<string> BuildStringList(string fp) => File.ReadAllLines(fp).ToList();
