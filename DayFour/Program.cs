97a90bcbe8ddb194ac1c92be00ebe4a552ecf4cb
Joshua Cook <jdcook3@outlook.com>
2020-12-04 08:22:55 -0600

    Stand up data

---
+++ DayFour/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+Console.WriteLine(ChallengeOne("Sample.txt"));
+Console.WriteLine(ChallengeOne("Input.txt"));
+Console.WriteLine(ChallengeTwo("Sample.txt"));
+Console.WriteLine(ChallengeTwo("Input.txt"));
+
+
+static int ChallengeOne(string filepath)
+{
+    int a = 0;
+
+
+    return a;
+}
+
+static int ChallengeTwo(string filepath)
+{
+    int a = 0;
+
+    return a;
+}
+
+static List<string> BuildStringList(string fp) => File.ReadAllLines(fp).ToList();
+
+class Passport
+{
+}
+
