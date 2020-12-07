04472ace4fa77f2b42427989c4a2d2b2874749cd
Joshua Cook <joshua.d.cook53.ctr@mail.mil>
2020-12-07 12:41:51 -0600

    Day 07 Initial Standup

---
+++ Day07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+Console.WriteLine(ChallengeOne("Sample.txt"));
+Console.WriteLine(ChallengeOne("Input.txt"));
+Console.WriteLine(ChallengeTwo("Sample.txt"));
+Console.WriteLine(ChallengeTwo("Input.txt"));
+
+
+long ChallengeOne(string filepath)
+{
+    return BuildStringList(filepath).Count();
+}
+
+long ChallengeTwo(string filepath)
+{
+    return BuildStringList(filepath).Count();
+}
+
+static List<string> BuildStringList(string fp) => File.ReadAllLines(fp).ToList();
+
+
+
