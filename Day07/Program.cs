3064268a955e76658f8c0feb928fe70657e0fa60
Joshua Cook <jdcook3@outlook.com>
2020-12-07 21:02:28 -0600

    Day Seven, Problem One

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+
+Regex containerRegex = new("^(.*) bags contain (.*).$");
+Regex containsRegex = new("(\\d+) (.*) bag");
 
 Console.WriteLine(ChallengeOne("Sample.txt"));
 Console.WriteLine(ChallengeOne("Input.txt"));
@@ -11,15 +15,60 @@ Console.WriteLine(ChallengeTwo("Input.txt"));
 
 long ChallengeOne(string filepath)
 {
-    return BuildStringList(filepath).Count();
+    Dictionary<string, Dictionary<string, int>> rules = BuildRuleSet(BuildStringList(filepath));
+
+    HashSet<string> answers = new(); 
+    GetContainersThatHold("shiny gold", answers, rules);
+    return answers.Count();
 }
 
 long ChallengeTwo(string filepath)
 {
-    return BuildStringList(filepath).Count();
+    Dictionary<string, Dictionary<string, int>> rules = BuildRuleSet(BuildStringList(filepath));
+    return rules.Count();
 }
 
-static List<string> BuildStringList(string fp) => File.ReadAllLines(fp).ToList();
+Dictionary<string, Dictionary<string, int>> BuildRuleSet(List<string> input)
+{
+    Dictionary<string, Dictionary<string, int>> rules = new();
+    input.ForEach(line =>
+    {
+        Match container = containerRegex.Match(line);
+        string containerColor = container.Groups[1].Value;
+        string contained = container.Groups[2].Value;
+
+        Dictionary<string, int> BagContents = new();
+        if(contained != "no other bags")
+        {
+            foreach(string bag in contained.Split(','))
+            {
+                Match bagInfo = containsRegex.Match(bag);
+
+                int count = int.Parse(bagInfo.Groups[1].Value);
+                string color = bagInfo.Groups[2].Value;
+
+                BagContents.Add(color, count);
+            }
+        }
+        rules.Add(containerColor, BagContents);
+    });
+
+    return rules;
+}
+
+void GetContainersThatHold(string color, HashSet<string> containers, Dictionary<string,Dictionary<string,int>> d)
+{
+    foreach(var container in d)
+    {
+        if(container.Value.ContainsKey(color))
+        {
+            containers.Add(container.Key);
+            if (d.TryGetValue(container.Key, out var keyValuePairs))
+                GetContainersThatHold(container.Key, containers, d);
+        }
+    }
+}
 
 
-
+static List<string> BuildStringList(string fp) => 
+    File.ReadAllLines(fp).Select(line => line.Trim()).ToList();
