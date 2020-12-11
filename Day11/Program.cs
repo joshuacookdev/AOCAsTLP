2ccb3dc2eefdef84d39f087ec1bf719953d1e136
Joshua Cook <jdcook3@outlook.com>
2020-12-10 22:50:12 -0600

    Initial Day 11

---
+++ Day11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+long ChallengeOne(string filepath)
+{
+    var data = BuildData(filepath);
+
+    return data.Count();
+}
+
+long ChallengeTwo(string filepath)
+{
+    var data = BuildData(filepath).ToHashSet();
+
+    return data.Count();
+}
+
+
+List<double> BuildData(string fp) => File.ReadAllLines(fp).Select(double.Parse).ToList();
+
+
+Console.WriteLine(ChallengeOne("Sample.txt"));
+Console.WriteLine(ChallengeOne("Input.txt"));
+Console.WriteLine(ChallengeTwo("Sample.txt"));
+Console.WriteLine(ChallengeTwo("Input.txt"));
