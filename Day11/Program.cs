7d25542fd0a29b17905fc6533f7fcc17e0bb11a9
Joshua Cook <jdcook3@outlook.com>
2020-12-11 00:50:20 -0600

    Day Eleven.
    I hate it.

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -4,25 +4,166 @@ using System.IO;
 using System.Linq;
 
 
-long ChallengeOne(string filepath)
+long Day11(string filepath, Func<List<List<bool?>>, int, int, bool?> evaluation)
 {
     var data = BuildData(filepath);
-
-    return data.Count();
+    List<List<bool?>> seatMap = BuildSeatMap(data);
+    int pre = 0, post = 1;
+    while (pre != post)
+    {
+        pre = seatMap.Sum(l => l.Count(i => i == true));
+        seatMap = ProcessSeats(seatMap, evaluation);
+        //PrintSeatMap(seatMap);
+        post = seatMap.Sum(l => l.Count(i => i == true));
+    }
+    return seatMap.Sum(l => l.Count(i => i == true));
 }
 
-long ChallengeTwo(string filepath)
+List<List<bool?>> BuildSeatMap(List<string> data)
 {
-    var data = BuildData(filepath).ToHashSet();
+    List<List<bool?>> results = new();
+    List<bool?> buffer = new bool?[data.First().Count() + 2].ToList();
+    results.Add(buffer);
+    data.ForEach(l =>
+    {
+        List<bool?> line = new();
+        line.Add(null);
+        foreach (char c in l)
+        {
+            line.Add(c == 'L' ? false : null);
+        }
+        line.Add(null);
+        results.Add(line);
+    });
+    results.Add(buffer);
+    return results;
+}
 
-    return data.Count();
+void PrintSeatMap(List<List<bool?>> map)
+{
+    map.ForEach(l =>
+    {
+        l.ForEach(v =>
+        {
+            if (v is null) Console.Write(".");
+            else if (v == true) Console.Write("#");
+            else Console.Write("L");
+        });
+        Console.WriteLine();
+    });
 }
 
 
-List<double> BuildData(string fp) => File.ReadAllLines(fp).Select(double.Parse).ToList();
+List<List<bool?>> ProcessSeats(List<List<bool?>> oldMap, Func<List<List<bool?>>, int, int, bool?> evaluation)
+{
+    List<List<bool?>> newMap = new();
+
+    for (int row = 0; row < oldMap.Count(); row++)
+    {
+        List<bool?> newRow = new();
+        for (int col = 0; col < oldMap.First().Count(); col++)
+        {
+            newRow.Add(evaluation(oldMap, row, col));
+        }
+        newMap.Add(newRow);
+    }
+
+    return newMap;
+}
+
+bool? EvaluateSeatOne(List<List<bool?>> oldMap, int row, int col)
+{
+    if (oldMap[row][col] is null) return null;
+    if (!(bool)oldMap[row][col]) // seat is empty
+        return SumSurroundingOne(oldMap, row, col) == 0 ? true : false;
+    else
+        return SumSurroundingOne(oldMap, row, col) < 4 ? true : false;
+}
 
 
-Console.WriteLine(ChallengeOne("Sample.txt"));
-Console.WriteLine(ChallengeOne("Input.txt"));
-Console.WriteLine(ChallengeTwo("Sample.txt"));
-Console.WriteLine(ChallengeTwo("Input.txt"));
+int SumSurroundingOne(List<List<bool?>> oldMap, int row, int col)
+{
+    int takenSeats = 0;
+    for (int i = -1; i <= +1; i++)
+    {
+        for (int j = -1; j <= +1; j++)
+        {
+            if ((i, j) == (0, 0)) continue;
+            var seat = oldMap[row + i][col + j];
+            if (seat is null)
+            {
+                continue;
+            }
+            takenSeats += (bool)seat ? 1 : 0;
+        }
+    }
+    return takenSeats;
+}
+
+bool? EvaluateSeatTwo(List<List<bool?>> oldMap, int row, int col)
+{
+    int xMax = oldMap.First().Count() - 1;
+    int yMax = oldMap.Count() - 1;
+
+    if (oldMap[row][col] is null) return null;
+    if (!(bool)oldMap[row][col]) // seat is empty
+        return SumSurroundingTwo(oldMap, row, col, xMax, yMax) == 0 ? true : false;
+    else
+        return SumSurroundingTwo(oldMap, row, col, xMax, yMax) < 5 ? true : false;
+}
+
+// 8 directions (and scales) to search for seats in Part 2
+List<(int x, int y)> directionalAddends = new()
+{
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+    (-1, 0),
+    (-1, 1)
+};
+
+int SumSurroundingTwo(List<List<bool?>> oldMap, int row, int col, int xMax, int yMax)
+{
+    
+    int takenSeats = 0;
+    
+    // Get Closest seat in each of 8 directions
+    foreach(var dAddend in directionalAddends)
+    {
+        int x = col, y = row;
+        do
+        {
+            x += dAddend.x;
+            y += dAddend.y;
+            if (x < 0 || x > xMax || y < 0 || y > yMax)
+            {
+                (x, y) = (0, 0);
+                break;
+            }
+        }
+        while (oldMap[y][x] == null);
+
+        // using 0,0 as a "I intentionally broke this" case, since I put a border of nulls around the chairs
+        // only evaluating if I know it made it out of the while loop organically.
+        if((x, y) != (0, 0))
+        {
+            bool? seat = oldMap[y][x];
+            takenSeats += (bool)seat ? 1 : 0;
+        }
+    }
+    return takenSeats;
+}
+
+
+
+
+List<string> BuildData(string fp) => File.ReadAllLines(fp).ToList();
+
+
+Console.WriteLine(Day11("Sample.txt", EvaluateSeatOne));
+Console.WriteLine(Day11("Input.txt", EvaluateSeatOne));
+Console.WriteLine(Day11("Sample.txt", EvaluateSeatTwo));
+Console.WriteLine(Day11("Input.txt", EvaluateSeatTwo));
