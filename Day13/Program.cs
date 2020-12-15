32f139c9da7ac980db6b815052d8708e76e07bbb
Joshua Cook <jdcook3@outlook.com>
2020-12-15 10:23:44 -0600

    Day 13 Part 2
    Cheated a bit once I realized all of the bus times were prime. Runs pretty quickly, if I do say so myself. I'm sure there is a more efficient big O notation alg, but this is better than what I started with.

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -3,38 +3,74 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+Console.WriteLine(ChallengeOne("Sample.txt"));
+Console.WriteLine(ChallengeOne("Input.txt"));
+Console.WriteLine(ChallengeTwo("Sample.txt"));
+Console.WriteLine(ChallengeTwo("Input.txt"));
 
-double ChallengeOne(string filepath)
+long ChallengeOne(string filepath)
 {
-    var data = BuildData(filepath);
-    int ArrivalTime = data.time;
-    int CurrentTime = ArrivalTime;
+    BusSchedule schedule = BuildDataOne(filepath);
+    long ArrivalTime = schedule.time;
+    long CurrentTime = ArrivalTime;
     while (true)
     {
-        foreach (int bus in data.buses)
+        foreach (Bus bus in schedule.buses)
         {
-            if (CurrentTime % bus == 0)
-                return bus * (CurrentTime-ArrivalTime);
+            if (CurrentTime % bus.number == 0)
+                return bus.number * (CurrentTime-ArrivalTime);
         }
         CurrentTime++;
     }
 }
 
-double ChallengeTwo(string filepath)
+long ChallengeTwo(string filepath)
 {
-    var data = BuildData(filepath);
-    return data.buses.Count();
+    List<Bus> buses = BuildDataTwo(filepath);
+    long time = 0; // no start time is described here, starting at T0
+    long increment = buses[0].number; // start with first bus in data
+
+    //iterate over other buses in data
+    foreach(Bus bus in buses.GetRange(1,buses.Count()-1))
+    {
+        long newTime = bus.number;
+        while (true)
+        {
+            time += increment;
+            if ((time + bus.tMinus) % newTime == 0)
+            {
+                increment *= newTime;
+                break;
+            }
+        }
+    }
+
+    return time;
 }
 
-(int time, List<int> buses) BuildData(string fp)
+BusSchedule BuildDataOne(string fp)
 {
     string[] data = File.ReadAllLines(fp);
     int time = int.Parse(data[0]);
-    List<int> buses = data[1].Split(',').Where(i => i != "x").Select(int.Parse).ToList();
-    return (time, buses);
+    return new BusSchedule(time, GetBuses(data[1]));
 }
 
-Console.WriteLine(ChallengeOne("Sample.txt"));
-Console.WriteLine(ChallengeOne("Input.txt"));
-Console.WriteLine(ChallengeTwo("Sample.txt"));
-Console.WriteLine(ChallengeTwo("Input.txt"));
+List<Bus> BuildDataTwo(string fp) => GetBuses(File.ReadAllLines(fp)[1]);
+
+List<Bus> GetBuses(string input)
+{
+    string[] data = input.Split(',');
+    List<Bus> buses = new();
+    for (int i = 0; i < data.Length; i++)
+    {
+        if (data[i] != "x")
+        {
+            buses.Add(new(long.Parse(data[i]), (long)i));
+        }
+    }
+    return buses;
+}
+
+public record BusSchedule(long time, List<Bus> buses) { }
+
+public record Bus(long number, long tMinus) { }
