c563ee067b34a9850a800a2d2a70f8b003fac7ac
Joshua Cook <jdcook3@outlook.com>
2020-12-14 22:27:09 -0600

    Day 13 Problem 1
    Have heard part 2 is hard so hopefully I made this simple enough to tear apart.

---
+++ Day13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+double ChallengeOne(string filepath)
+{
+    var data = BuildData(filepath);
+    int ArrivalTime = data.time;
+    int CurrentTime = ArrivalTime;
+    while (true)
+    {
+        foreach (int bus in data.buses)
+        {
+            if (CurrentTime % bus == 0)
+                return bus * (CurrentTime-ArrivalTime);
+        }
+        CurrentTime++;
+    }
+}
+
+double ChallengeTwo(string filepath)
+{
+    var data = BuildData(filepath);
+    return data.buses.Count();
+}
+
+(int time, List<int> buses) BuildData(string fp)
+{
+    string[] data = File.ReadAllLines(fp);
+    int time = int.Parse(data[0]);
+    List<int> buses = data[1].Split(',').Where(i => i != "x").Select(int.Parse).ToList();
+    return (time, buses);
+}
+
+Console.WriteLine(ChallengeOne("Sample.txt"));
+Console.WriteLine(ChallengeOne("Input.txt"));
+Console.WriteLine(ChallengeTwo("Sample.txt"));
+Console.WriteLine(ChallengeTwo("Input.txt"));
